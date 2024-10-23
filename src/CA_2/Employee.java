0f212e2a497dc8bb1fb3e90f2eb21a8db68762b0
Mikel Pinto <2024044@student.cct.ie>
2024-10-23 20:38:47 +0100

    Created constructors for the new variable that was created for the class Employee

--- src/CA_2/Employee.java
+++ src/CA_2/Employee.java
@@ -4,31 +4,59 @@
  */
 package CA_2;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 /**
  *
  * @author Mikel
  */
-public class Employee{
+public class Employee {
 
-    private String name;
-    private LocalDate dob;
-    private String address;
-  
+    private final String id;
+    private final String name;
+    private final LocalDate dob;
+    private final String address;
+    private final BigDecimal salary;
+    private final String departmentId;
+    private final String positionId;
 
-    public Employee(String name) {
+    public Employee(String id, String name, LocalDate dob, String address, BigDecimal salary, String departmentId, String positionId) {
+        this.id = id;
         this.name = name;
         this.dob = dob;
+        this.address = address;
+        this.salary = salary;
+        this.departmentId = departmentId;
+        this.positionId = positionId;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public String getDepartmentId() {
+        return positionId;
+    }
+
+    public String getPositionId() {
+        return positionId;
     }
 
 }
